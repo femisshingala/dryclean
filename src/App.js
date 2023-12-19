49643e645d55511302d27e93b9d753755ac56d9a
Femis <femisshingala54@gmail.com>
2023-12-19 16:11:00 +0530

    f1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ShopRegister from './componets/ShopRegister/ShopRegister'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   
+      <ShopRegister />
+      
+ 
+  )
 }
 
-export default App;
+export default App
+
+// import React, { useState } from 'react';
+// import './App.css'; // Import your CSS file
+
+// const App = () => {
+  
+
+//   return (
+//     <div className={`box ${isActive ? 'active' : ''}`} onClick={toggleAnimation}>
+//       Click me
+//     </div>
+//   );
+// };
+
+// export default App;
